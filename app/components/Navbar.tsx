5118564df9cb598daf13110aaa067d698681bd53
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-11-11 13:02:24 +0530

    working on auth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { auth, signOut, signIn } from "@/auth";
+
+const Navbar = async () => {
+    const session = await auth();
+    
+    return(
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                </Link>
+
+                <div className="flex items-center gap-5 text-black">
+                    { session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span className="max-sm:hidden">Create</span>
+                                <BadgePlus className="size-6 sm:hidden" />
+                            </Link>
+
+                            <form
+                                action = {async () => {
+                                    "use server";
+
+                                    await signOut({redirectTo: "/"});
+                                }}
+                            >
+                                <button type="submit">
+                                    <span className="max-sm:hidden">Logout</span>
+                                    <LogOut className="size-6 sm:hidden text-red-500" />
+                                </button>
+                            </form>
+                        </>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
