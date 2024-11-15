a1e2c2f536b3d2150e89da64db3250d403cf6a7e
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-11-15 12:09:39 +0530

    working on components

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,26 +1,71 @@
 import Link from 'next/link';
 import {EyeIcon} from "lucide-react";
 import {cn, formatDate} from "@/lib/utils";
+import {Button} from "@/components/ui/button";
+import Image from "next/image";
+
+type StartupTypeCard = {
+    _createdAt: string;
+    views: number;
+    author: {
+        id: string;
+        name: string;
+    };
+    title: string;
+    category: string;
+    _id: string;
+    image: string;
+    description: string;
+};
+
 
 const StartupCard = ({ post }: {post: StartupTypeCard}) => {
+    const { _createdAt, views, author: { id: authorId, name }, title, category, _id, image, description } = post;
+
+    // @ts-ignore
     return(
         <li className="startup-card group">
             <div className="flex-between">
                 <p className="startup_card_date">
-                    {formatDate(post._createdAt)}
+                    {formatDate(_createdAt)}
                 </p>
                 <div className="flex gap-1.5">
                     <EyeIcon className="size-6 text-primary" />
-                    <span className="text-16-medium">{post.views}</span>
+                    <span className="text-16-medium">{views}</span>
                 </div>
             </div>
 
             <div className="flex-between mt-5 gap-5">
                 <div className="flex-1">
-                    <Link href={`/user/${post.author?._id}`}>
-                        <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+                    <Link href={`/user/${authorId}`}>
+                        <p className="text-16-medium line-clamp-1">{name}</p>
+                    </Link>
+                    <Link href={`/startup/${_id}`}>
+                        <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
                 </div>
+                <Link href={`/user/${authorId}`}>
+                    <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
+                </Link>
+            </div>
+
+            <Link href={`/startup/${_id}`}>
+                <p className="startup-card_desc">
+                    {description}
+                </p>
+
+                <img src={image} alt="placeholder" className="startup-card_image" />
+            </Link>
+
+            <div className="flex-between gap-3 mt-5">
+                <Link href={`/?query=${category.toLowerCase()}`}>
+                    <p className="text-16-medium">{category}</p>
+                </Link>
+                <Button className="startup-card_btn" asChild>
+                    <Link href={`/startup/${_id}`}>
+                        Details
+                    </Link>
+                </Button>
             </div>
         </li>
     )
